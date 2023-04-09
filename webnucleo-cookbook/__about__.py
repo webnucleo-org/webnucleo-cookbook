f95c508e9227608f368724f65c474d326d9ec5a3
Bradley Meyer <Brad@Elizabeths-Air.attlocal.net>
2023-04-09 09:27:37 -0400

    Update.

--- webnucleo-cookbook/__about__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["__title__", "__summary__", "__version__", "__author__", "__copyright__"]
-
-__title__ = "Webnucleo Cookbook"
-__summary__ = "Cookbook to illustrate Webnucleo resources."
-__version__ = "1.0.0"
-__author__ = "Clemson University"
-__copyright__ = "Clemson University, 2023"
